d9cadae835a0359669e9d3f16beb2315bcf75017
josevalenzuela <pepe13.2.25@gmail.com>
2020-09-13 13:23:44 -0600

    Estructura del proyecto

---
+++ src/main/java/com/josevalenzuela/apiprospectos/ApiProspectosApplication.java
@@ -0,0 +1,13 @@
+package com.josevalenzuela.apiprospectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProspectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProspectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josevalenzuela/apiprospectos/ApiProspectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josevalenzuela.apiprospectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProspectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
